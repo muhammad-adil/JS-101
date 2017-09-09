5b6dc9d48fd88dddd3e43f993bf52227c8b49bf8
unknown <adil.muhammad93@gmail.com>
2017-09-09 14:00:00 +0100

    ES5 .filter array

---
+++ JS/js-filter.js
@@ -0,0 +1,9 @@
+const people = [
+	{ name: 'pac' , age: 21 },
+	{ name: 'jack' , age: 31 },
+	{ name: 'john' , age: 26 },
+];
+
+const filteredeople = people.filter(person => person.age > 25 );
+
+console.log(filteredPeople);
