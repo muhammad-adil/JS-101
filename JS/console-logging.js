051490eded2d58af75c76b273de3270633ee89fe
Muhammad-Adil <adil.muhammad93@gmail.com>
2017-11-11 18:28:30 +0500

    console logging add

--- JS/console-logging.js
+++ JS/console-logging.js
@@ -42,5 +42,9 @@ for(var i = 0; i < 100; i++){
 	console.log(i);
 	console.groupEnd();	
 	console.groupEnd();
-
 }
+
+
+//console.assert
+var foo = undefined;
+console.assert(foo, "Foo wasn't set");
