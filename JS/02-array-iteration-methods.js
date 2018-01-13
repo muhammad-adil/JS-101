642c30676e667f2903ddd9a8649c267ac219f96a
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-01-13 22:20:30 +0500

    array iteration methods

--- JS/02-array-iteration-methods.js
+++ JS/02-array-iteration-methods.js
@@ -56,7 +56,11 @@ console.log(found);
 
 
 // find index
-
+// const objects = [{id: 'a'}, {id: 'b'}, {id: 'c'}];
+const foundIndex = objects.findIndex(function(item){
+	return item.id === 'b';
+});
+console.log(foundIndex);
 
 
 
