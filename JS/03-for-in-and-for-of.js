5c89f2f80f8475e0ae16a6f420522efa4cf15da4
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-01-14 22:20:30 +0500

    for in and for of

---
+++ JS/03-for-in-and-for-of.js
@@ -0,0 +1,16 @@
+
+/*	for in / for of*/
+
+//usage
+for(variable in object){
+	statements
+}
+
+for(variable in object){
+	statement
+}
+
+let person = {firstName: "John", lastName: "Doe", arms: 2};
+
+let arr = [3,5,7];
+arr.foo = 'hello';
