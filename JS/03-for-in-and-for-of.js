6cdf8ae7108707c805f379a52f4d8cb7e74f71b9
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-01-14 22:20:30 +0500

    for in and for of

--- JS/03-for-in-and-for-of.js
+++ JS/03-for-in-and-for-of.js
@@ -1,16 +1,55 @@
 
 /*	for in / for of*/
 
-//usage
-for(variable in object){
-	statements
-}
+// for in [loop through property names];
+// for of [loop through property values ]
 
-for(variable in object){
-	statement
-}
+
+//usage
+
+// for(variable in object){
+// 	statements
+// }
+
+
+// for(variable in object){
+// 	statement
+// }
 
 let person = {firstName: "John", lastName: "Doe", arms: 2};
 
 let arr = [3,5,7];
 arr.foo = 'hello';
+
+let text = "";
+foo(let x in person){
+	text += person[x];
+	console.log(x);
+};
+console.log(text);
+
+/*
+for of loop will not work on person because person
+is not iterable,
+the for in loops through innumerable properties while for
+of loops through iterable objects such as array map & sets.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
