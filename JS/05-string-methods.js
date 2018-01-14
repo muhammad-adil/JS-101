926107248ceb23c6fa0ef6b1ca60e3cf784eacbc
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-01-14 12:50:23 +0500

    common string methods

---
+++ JS/05-string-methods.js
@@ -0,0 +1,53 @@
+
+/* Common String methods */
+
+var stringOne = "freeCodeCamp is the best place to learn";
+var stringTwo = "frontend and backend development";
+
+
+// charAt(); //returns character at index
+console.log(stringOne.charAt(1));
+
+
+// charCodeAt(); //returns unicode character at index
+console.log(stringOne.charCodeAt(1));
+
+
+// concat() //joins two or more strings and return a new string
+console.log(stringOne.concat(stringTwo));
+
+
+// endsWith()
+console.log(stringOne.endsWith("to")) //false
+
+
+// fromCharCode()
+console.log(stringOne.fromCharCode(114));
+
+
+// includes()
+console.log(stringTwo.includes("end")) //true
+
+// indexOf()
+
+
+// lastIndexOf()
+
+
+// match()
+
+
+// repeat()
+
+// replace()
+
+
+
+
+
+
+
+
+
+
+
